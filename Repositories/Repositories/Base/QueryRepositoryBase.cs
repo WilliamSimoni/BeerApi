dfe9ca0b957b72381df91ec0dc4b63490cc02d28
William Simoni <wilsimoni@gmail.com>
2022-08-18 10:33:13 +0200

    Implemented Base repositories

---
+++ Repositories/Repositories/Base/QueryRepositoryBase.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+using Repositories.DataContext;
+using System.Linq.Expressions;
+
+namespace Repositories.Repositories.Base
+{
+    public class QueryRepositoryBase<T> : IQueryRepositoryBase<T> where T : class
+    {
+
+        private readonly DbSet<T> _entities;
+
+        public QueryRepositoryBase(AppDbContext context)
+        {
+            _entities = context.Set<T>();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _entities;
+        }
+
+        public IQueryable<T> GetByCondition(Expression<Func<T, bool>> condition)
+        {
+            return _entities.Where(condition);
+        }
+    }
+}
